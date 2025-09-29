52eb5307419510ea9e0368d703b242a0673ca1cd
Shreeya <shreeyap@metropolia.fi>
2025-09-29 13:48:54 +0300

    Iteration 2 complete

--- week7-fepp-starter/frontend/src/pages/HomePage.jsx
+++ week7-fepp-starter/frontend/src/pages/HomePage.jsx
@@ -1,9 +1,21 @@
 import JobListings from "../components/JobListings";
+import {useEffect, useState} from "react";
 
 const Home = () => {
+  const [jobs, setJobs] = useState([]);
+  
+  useEffect(() => {
+    const fetchJobs= async () =>{
+      const res = await fetch("/api/jobs");
+      const data = await res.json();
+      setJobs(data);
+  }
+  fetchJobs();
+},[]);
+
   return (
     <div className="home">
-      <JobListings  />
+      <JobListings jobs={jobs} />
     </div>
   );
 };
